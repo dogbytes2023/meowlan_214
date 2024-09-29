0af2d24cbc20dfd2d563875751ce44bc49884515
shrimp <ethandakin@outlook.com.au>
2024-09-29 10:33:33 +1000

    added springboot initialization

---
+++ flydreamair/src/main/java/edu/meowlan/flydreamair/FlydreamairApplication.java
@@ -0,0 +1,13 @@
+package edu.meowlan.flydreamair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlydreamairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlydreamairApplication.class, args);
+	}
+
+}
---
+++ flydreamair/src/test/java/edu/meowlan/flydreamair/FlydreamairApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.meowlan.flydreamair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlydreamairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
